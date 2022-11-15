14e9e6692104526d1004c364ff0329ccadc2f444
cmoradi06 <49164902+cmoradi06@users.noreply.github.com>
2022-11-15 08:00:23 -0600

    Update condCARDS.cpp

--- condCARDS.cpp
+++ condCARDS.cpp
@@ -17,28 +17,28 @@ void condCARDS(int randZ) {
     }
     else 
       if (randA <= 85) {
-       jackCARDS += 150;
-      std::cout << "you got back 150 jack cards";
-      }
-      else if (randA <= 90) {
-        jackCARDS += 200;
-        std::cout << "you got back 200 jack cards";
+        jackCARDS += 150;
+        std::cout << "you got back 150 jack cards";
         }
-        else if (randA <= 94) {
-          jackCARDS += 250;
-          std::cout << "you got back 250 jack cards";
+        else if (randA <= 90) {
+          jackCARDS += 200;
+          std::cout << "you got back 200 jack cards";
           }
-          else if (randA <= 97) {
-            jackCARDS += 500;
-            std::cout << "you got back 5 king cards (500 jack cards)";
+          else if (randA <= 94) {
+            jackCARDS += 250;
+            std::cout << "you got back 250 jack cards";
             }
-            else if (randA <= 99) {
-              jackCARDS += 1000;
-             std::cout << "you got back 10 king cards (1000 jack cards)";
-            }
-            else {
+            else if (randA <= 97) {
+              jackCARDS += 500;
+              std::cout << "you got back 5 king cards (500 jack cards)";
+              }
+              else if (randA <= 99) {
+                jackCARDS += 1000;
+                std::cout << "you got back 10 king cards (1000 jack cards)";
+              }
+              else {
               jackCARDS += 2000;
               std::cout << "you got back 20 king cards (2000 jack cards)";
-            }
+              }
 
 }
