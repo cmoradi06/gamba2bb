43ab9d9ac5e43c76b3999d25808e31fda9a2154d
cmoradi06 <49164902+cmoradi06@users.noreply.github.com>
2022-11-15 07:08:41 -0600

    Create randNUMBER.cpp

---
+++ randNUMBER.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>      /* printf, scanf, puts, NULL */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>
+#include <ostream>
+
+int main();
+void condHAIR(int randY);
+
+int randNUMBER() {
+  int randN;
+  randN = rand() % 100 + 1;
+  return randN;
+
+}
