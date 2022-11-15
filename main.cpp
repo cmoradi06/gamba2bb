93391d9c8cbfd94eb0acf96302756df55feb4ec3
cmoradi06 <49164902+cmoradi06@users.noreply.github.com>
2022-11-15 07:46:50 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>      /* printf, scanf, puts, NULL */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>
+#include <ostream>
+
+int randNUMBER();
+void condCARDS(int randY);
+int jackCARDS;
+
+int main() {
+  jackCARDS = 2000;
+  srand (time(0));
+  for (int iA = 1; iA < (20 + 1); iA++) {
+    int randD;
+    randD = randNUMBER();
+    std::cout << "random number: ";
+    std::cout << randD << " => ";
+    condCARDS(randD);
+    std::cout << std::endl;
+  }
+
+}
